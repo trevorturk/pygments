829b7bc4dc1eadcb509d17dc74256c30666ffafd
Trevor Turk <trevorturk@gmail.com>
2009-09-15 01:31:15 -0500

    update readme, refactor a bit

--- app.py
+++ app.py
@@ -5,26 +5,20 @@ from pygments import highlight
 from pygments.lexers import get_lexer_by_name
 from pygments.formatters import HtmlFormatter
 
-class Index(webapp.RequestHandler):
+class App(webapp.RequestHandler):
   def get(self):
     self.response.headers['Content-Type'] = 'text/plain'
-    self.response.out.write(readme())
+    self.response.out.write(open('README', 'r').read())
   def post(self):
-    lang = self.request.get("lang")
-    code = self.request.get("code")
     self.response.headers['Content-Type'] = 'text/html'
-    self.response.out.write(pygmentize(lang, code))
+    self.response.out.write(pygmentize(self.request.get("lang"), self.request.get("code")))
 
-application = webapp.WSGIApplication([('/', Index)], debug=True)
+application = webapp.WSGIApplication([('/', App)], debug=True)
 
 def pygmentize(lang, code):
   lexer = get_lexer_by_name(lang, stripall=True)
   formatter = HtmlFormatter(linenos=True, cssclass="highlight")
   return highlight(code, lexer, formatter)
-
-def readme():
-  f = open('README', 'r')
-  return f.read()
 
 def main():
   run_wsgi_app(application)
